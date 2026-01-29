19c962186aa6c8ee6040671890b67af5720bca39
Sabritocaster <sabrierend@gmail.com>
2026-01-29 21:19:18 +0300

    fixed image paths

--- data/projects.js
+++ data/projects.js
@@ -6,7 +6,7 @@ export const projects = [
         category: 'Branding',
         description: 'Automotive Efficiency Solutions',
         slug: 'brand1',
-        image: '/yavuz-portfolio/images/Branding/1_Most/Most_Thumbnail_Branding.jpg',
+        image: '/images/Branding/1_Most/Most_Thumbnail_Branding.jpg',
         colSpan: 6,
         longDescription: 'Brand identity design for Most, an automotive efficiency solutions company.',
         details: [],
@@ -19,17 +19,17 @@ export const projects = [
         description: 'Middle East Exhibition Platform',
         slug: 'brand2',
         date: '2025',
-        image: '/yavuz-portfolio/images/Branding/2_Düş/Düş_Thumbnail_Branding.jpg',
+        image: '/images/Branding/2_Düş/Düş_Thumbnail_Branding.jpg',
         colSpan: 3,
         longDescription: 'Identity design for Düş, a contemporary art platform focused on Middle Eastern art. The project features a cohesive visual system applied to event tickets and promotional assets, blending sharp and clear sensibilities with a focus on cultural exchange.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/2_Düş/Düş1.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/2_Düş/Düş2_EXTENDED.jpg', isFullscreen: true },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/2_Düş/Düş3.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/2_Düş/Düş4.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/2_Düş/Düş5.gif', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/2_Düş/Düş6.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/2_Düş/Düş7.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/2_Düş/Düş1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/2_Düş/Düş2_EXTENDED.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Branding/2_Düş/Düş3.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/2_Düş/Düş4.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/2_Düş/Düş5.gif', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/2_Düş/Düş6.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/2_Düş/Düş7.jpg', isFullscreen: false },
         ]
     },
     {
@@ -38,19 +38,19 @@ export const projects = [
         category: 'Branding',
         description: 'A Comprehensive Biodiversity Museum',
         slug: 'brand3',
-        image: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik_Thumbnail_Branding.jpg',
+        image: '/images/Branding/3_Biyocesitlilik/Biyocesitlik_Thumbnail_Branding.jpg',
         colSpan: 3,
         longDescription: 'Commissioned by the Hacettepe Biosphere Museum to renew its identity, this project establishes a modular system rooted in scientific classification. Based on the museum\'s categories, various branches were visualized through unique patterns, creating a flexible identity that transforms within the exhibition context.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik1.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik2.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik3_GIF.gif', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik4_EXTENDED.jpg', isFullscreen: true },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik5.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik6_EXTENDED.jpg', isFullscreen: true },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik7_GIF.gif', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik8.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Branding/3_Biyocesitlilik/Biyocesitlik9.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik2.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik3_GIF.gif', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik4_EXTENDED.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik5.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik6_EXTENDED.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik7_GIF.gif', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik8.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Branding/3_Biyocesitlilik/Biyocesitlik9.jpg', isFullscreen: false },
         ]
     },
 
@@ -61,12 +61,12 @@ export const projects = [
         category: 'Editorial',
         description: 'Book Cover Design',
         slug: 'edit1',
-        image: '/yavuz-portfolio/images/Editorial/1_Leviathan/Leviathan_Thumbnail_Editorial.png',
+        image: '/images/Editorial/1_Leviathan/Leviathan_Thumbnail_Editorial.png',
         colSpan: 3,
         longDescription: 'A conceptual book cover for Thomas Hobbes\' Leviathan, exploring the themes of social contracts and sovereign order. The design uses a series of eyes representing individual humans that together form a unified identity. This visual metaphor symbolizes the creation of order and the collective state as described in the text.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Editorial/1_Leviathan/Leviathan1.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Editorial/1_Leviathan/Leviathan2.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Editorial/1_Leviathan/Leviathan1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Editorial/1_Leviathan/Leviathan2.jpg', isFullscreen: true },
         ]
     },
     {
@@ -75,12 +75,12 @@ export const projects = [
         category: 'Editorial',
         description: 'Book Cover Design',
         slug: 'edit2',
-        image: '/yavuz-portfolio/images/Editorial/2_TheGrapesOfWrath/TheGrapesOfWratth_Thumbnail_Editorial.png',
+        image: '/images/Editorial/2_TheGrapesOfWrath/TheGrapesOfWratth_Thumbnail_Editorial.png',
         colSpan: 3,
         longDescription: 'A contemporary reimagining of John Steinbeck\'s classic novel, utilizing a Constructivist aesthetic. By combining Alexander Rodchenko\'s typography with Dorothea Lange\'s photography, the design captures both the author\'s political undertones and the historical context of the Great Depression.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Editorial/2_TheGrapesOfWrath/TheGrapesOfWratth1.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Editorial/2_TheGrapesOfWrath/TheGrapesOfWratth2.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Editorial/2_TheGrapesOfWrath/TheGrapesOfWratth1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Editorial/2_TheGrapesOfWrath/TheGrapesOfWratth2.jpg', isFullscreen: true },
         ]
     },
     {
@@ -89,13 +89,13 @@ export const projects = [
         category: 'Editorial',
         description: 'A Visual Encyclopedia',
         slug: 'edit3',
-        image: '/yavuz-portfolio/images/Editorial/3_Encylopedia/Encylopedia_Thumbnail_Editorial_GIF.gif',
+        image: '/images/Editorial/3_Encylopedia/Encylopedia_Thumbnail_Editorial_GIF.gif',
         colSpan: 6,
         longDescription: 'This project provides a visual record of significant human migrations across different continents. Each global movement is categorized and symbolized by a specific color-coding system, creating a navigational structure. The layout integrates historical data with custom graphics, maps, and photography to explain these journeys.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Editorial/3_Encylopedia/Encylopedia1.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Editorial/3_Encylopedia/Encylopedia2.jpg', isFullscreen: true },
-            { type: 'image', src: '/yavuz-portfolio/images/Editorial/3_Encylopedia/Encylopedia3.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Editorial/3_Encylopedia/Encylopedia1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Editorial/3_Encylopedia/Encylopedia2.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Editorial/3_Encylopedia/Encylopedia3.jpg', isFullscreen: false },
         ]
     },
 
@@ -106,14 +106,14 @@ export const projects = [
         category: 'Poster',
         description: 'Bilkent Fine Arts Exhibition',
         slug: 'post1',
-        image: '/yavuz-portfolio/images/Poster/1_FragmentsOfLife/FragmentsOfLife_Thumbnail_Poster_GIF.gif',
+        image: '/images/Poster/1_FragmentsOfLife/FragmentsOfLife_Thumbnail_Poster_GIF.gif',
         colSpan: 4,
         isFullWidth: false,
         longDescription: 'Commissioned by the Bilkent Fine Arts department, this project involved developing a visual identity that symbolizes the fragments of human experience. The resulting series of posters maintains a unified identity while showcasing various interpretations. This was a collaborative project created by Melih Uçan, Reyyan Fidan, Erdem Bozdemir, and Yavuz Dağdelen.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Poster/1_FragmentsOfLife/FragmentsOfLife1.jpg', isFullscreen: true },
-            { type: 'image', src: '/yavuz-portfolio/images/Poster/1_FragmentsOfLife/FragmentsOfLife2.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Poster/1_FragmentsOfLife/FragmentsOfLife3_EXTENDED.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Poster/1_FragmentsOfLife/FragmentsOfLife1.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Poster/1_FragmentsOfLife/FragmentsOfLife2.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Poster/1_FragmentsOfLife/FragmentsOfLife3_EXTENDED.jpg', isFullscreen: true },
         ]
     },
     {
@@ -122,12 +122,12 @@ export const projects = [
         category: 'Poster',
         description: 'Visual Communication and Engagement',
         slug: 'post2',
-        image: '/yavuz-portfolio/images/Poster/2_Club/Club_Thumbnail_Poster_GIF.gif',
+        image: '/images/Poster/2_Club/Club_Thumbnail_Poster_GIF.gif',
         colSpan: 4,
         longDescription: 'Created for the Bilkent Graphic Design and Typography Society, these posters were designed to maximize event attendance through attention-grabbing visuals. To effectively attract attention from a distance, the series uses bold typography and minimalist layouts, delivering clear information through simplicity often developed in short timeframes.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Poster/2_Club/Club1.jpg', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Poster/2_Club/Club2_EXTENDED.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Poster/2_Club/Club1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Poster/2_Club/Club2_EXTENDED.jpg', isFullscreen: true },
         ]
     },
     {
@@ -136,11 +136,11 @@ export const projects = [
         category: 'Poster',
         description: 'Typographic Poster Project',
         slug: 'post3',
-        image: '/yavuz-portfolio/images/Poster/3_SibelEgemen/SibelEgemen_Thumbnail_Poster.png',
+        image: '/images/Poster/3_SibelEgemen/SibelEgemen_Thumbnail_Poster.png',
         colSpan: 4,
         longDescription: 'Developed as part of a typographic course assignment, this poster pays tribute to artist Sibel Egemen. The design captures a nostalgic atmosphere that reflects her musical identity, combining visual depth with a commitment to simplicity.',
         details: [
-            { type: 'image', src: '/yavuz-portfolio/images/Poster/3_SibelEgemen/SibelEgemen1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Poster/3_SibelEgemen/SibelEgemen1.jpg', isFullscreen: false },
         ]
     },
 
@@ -151,12 +151,12 @@ export const projects = [
         category: 'Motion',
         description: 'Motion Branding Study',
         slug: 'motion1',
-        image: '/yavuz-portfolio/images/Motion/1_Sqaurespace/Squarespace_Thumbnail_Motio_GIF.gif',
+        image: '/images/Motion/1_Sqaurespace/Squarespace_Thumbnail_Motio_GIF.gif',
         colSpan: 4,
         longDescription: 'Created for a motion design course, this project focuses on a logo animation that embodies the brand\'s core identity. The animation visualizes the concept of responsiveness across web and mobile platforms, fluidly transforming into the final Squarespace logo. The sound design for this piece was composed by Başarasa.',
         details: [
             { type: 'video', vimeoId: '1157896469', title: 'Squarespace Logo Animation', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Motion/1_Sqaurespace/Squarespace1.jpg', isFullscreen: false },
+            { type: 'image', src: '/images/Motion/1_Sqaurespace/Squarespace1.jpg', isFullscreen: false },
         ]
     },
     {
@@ -165,7 +165,7 @@ export const projects = [
         category: 'Motion',
         description: 'Kinetic Typography Project',
         slug: 'motion2',
-        image: '/yavuz-portfolio/images/Motion/2_KineticTypography/KineticTypography_Thumbnail_Motio_GIF.gif',
+        image: '/images/Motion/2_KineticTypography/KineticTypography_Thumbnail_Motio_GIF.gif',
         colSpan: 4,
         longDescription: 'This project explores the features of Adobe After Effects through a visual reinterpretation of Radiohead\'s "Faust Arp." The design utilizes the England signage font—featured on the original album cover—to create a dynamic, continuous typographic flow that is synchronized with the song\'s rhythm and feeling.',
         details: [
@@ -178,13 +178,13 @@ export const projects = [
         category: 'Motion',
         description: 'Cell Animation Course Project',
         slug: 'motion3',
-        image: '/yavuz-portfolio/images/Motion/3_Inky/Inky_Thumbnail_Motion_GIF.gif',
+        image: '/images/Motion/3_Inky/Inky_Thumbnail_Motion_GIF.gif',
         colSpan: 4,
         longDescription: 'This project aims to show the function of the ink tool in an engaging way. I developed the character "Inky" to perform a rhythmic dance while showing the tool\'s purpose. These frames were hand-drawn, blending traditional animation techniques with digital tools.',
         details: [
             { type: 'video', vimeoId: '1157894926', title: 'Creativity in Each Drop', isFullscreen: false },
-            { type: 'image', src: '/yavuz-portfolio/images/Motion/3_Inky/Inky1_EXTENDED.jpg', isFullscreen: true },
-            { type: 'image', src: '/yavuz-portfolio/images/Motion/3_Inky/Inky2_EXTENDED.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Motion/3_Inky/Inky1_EXTENDED.jpg', isFullscreen: true },
+            { type: 'image', src: '/images/Motion/3_Inky/Inky2_EXTENDED.jpg', isFullscreen: true },
         ]
     },
 
@@ -195,7 +195,7 @@ export const projects = [
         category: 'Personal',
         description: 'Digital Painting, 2024',
         slug: 'personal1',
-        image: '/yavuz-portfolio/images/Personal/1.jpg',
+        image: '/images/Personal/1.jpg',
         colSpan: 4,
         longDescription: 'A digital painting exploring the relationship between two lovers, Ömer and Macide.',
         details: [],
@@ -207,7 +207,7 @@ export const projects = [
         category: 'Personal',
         description: 'Color Study, 2025',
         slug: 'personal2',
-        image: '/yavuz-portfolio/images/Personal/2.jpg',
+        image: '/images/Personal/2.jpg',
         colSpan: 4,
         longDescription: 'A color study exploring light and shadow through the relationship between a boy and a lamp.',
         details: [],
@@ -219,7 +219,7 @@ export const projects = [
         category: 'Personal',
         description: 'Color Study on Dorothea Lange\'s Photo, 2026',
         slug: 'personal3',
-        image: '/yavuz-portfolio/images/Personal/3.jpg',
+        image: '/images/Personal/3.jpg',
         colSpan: 4,
         longDescription: 'A color study based on Dorothea Lange\'s iconic photography, exploring the visual language of documentary photography.',
         details: [],
